2e5b8592acc3706b9d667576884d3c8fd90064c1
Krunk Zhou <26049401+KrunkZhou@users.noreply.github.com>
2026-08-27 19:49:32 -0400

    Retain custom longitudinal cruise acceleration tables

--- openpilot/selfdrive/controls/lib/longitudinal_planner.py
+++ openpilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -17,9 +17,9 @@ from openpilot.common.swaglog import cloudlog
 
 from openpilot.sunnypilot.selfdrive.controls.lib.longitudinal_planner import LongitudinalPlannerSP
 
-A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
-A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
-J_CRUISE_VALS = [1.6, 1.2, 0.8, 0.6]
+A_CRUISE_MAX_VALS = [2.0, 2.0, 1.95, 1.60, 1.30, 1.05, 0.75]
+A_CRUISE_MAX_BP = [0., 5., 10., 15., 20., 25., 40.]
+J_CRUISE_VALS = [2.0, 2.0, 1.95, 1.60, 1.30, 1.05, 0.75]
 A_CRUISE_MIN = -1.2
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 ALLOW_THROTTLE_THRESHOLD = 0.4
